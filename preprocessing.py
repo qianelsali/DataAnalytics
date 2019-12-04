2e229699d792a8f5ae4f5e2c7c67e828bed5cf43
qianelsali <qianelsali@gmail.com>
2019-12-04 14:26:04 -0800

    add summry

---
+++ preprocessing.py
@@ -0,0 +1,5 @@
+summary = pd.DataFrame({'Valid(count)':df.count(),
+'Missing(count)':df.isna().sum(),
+'Missing(%)': (df.isna().sum()/len(df)).map(lambda x: '{:.00%}'.format(x)),
+})
+print(summary)
