7e637bcfb0b4688b11c135c35adbf72d24792201
qianelsali <onehorselee@gmail.com>
2019-12-04 15:08:52 -0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
-summary = pd.DataFrame({'Valid(count)':df.count(),
-'Missing(count)':df.isna().sum(),
-'Missing(%)': (df.isna().sum()/len(df)).map(lambda x: '{:.00%}'.format(x)),
-})
-print(summary)
+def checkMissingValues(df):
+    summary = pd.DataFrame({'Valid(count)':df.count(),
+                            'Missing(count)':df.isna().sum(),
+                            'Missing(%)': (df.isna().sum()/len(df)).map(lambda x: '{:.00%}'.format(x)),
+                            })
+    print(summary)
