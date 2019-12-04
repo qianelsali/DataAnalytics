a3269f94dafea2fa5005022faf32f456721364dc
qianelsali <onehorselee@gmail.com>
2019-12-04 15:09:40 -0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,5 @@
+## pipeline for data preprocessing
+
 def checkMissingValues(df):
     summary = pd.DataFrame({'Valid(count)':df.count(),
                             'Missing(count)':df.isna().sum(),
