9d1430cab874d5d3fa492138b34a7143cbd2d3c7
qianelsali <onehorselee@gmail.com>
2019-12-05 11:45:52 -0800

    Create writeS3.py

---
+++ writeS3.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+import time
+
+os.makedirs(os.path.dirname('logs/'), exist_ok=True)
+applog_filename = "logs/app_log.log"
+reclog_filename = "logs/request_rec.txt"
+logging.basicConfig(filename=applog_filename, level=logging.DEBUG)
+BUCKET_NAME = 'application-logs'
+		
+		
+def S3_helper(SOURCE_FILENAME, BUCKET_NAME, TARGET_FILENAME):
+    S3 = boto3.client('s3')
+    S3.upload_file(SOURCE_FILENAME, BUCKET_NAME, TARGET_FILENAME)
+    
+    
+def write_logs_S3():
+		SourceFileDict = {applog_filename: "app_log/"+applog_filename,
+					reclog_filename: "request_rec/"+reclog_filename}
+		try:
+			for source_filename in SourceFileDict:
+				now = time.time()
+				S3_helper(source_filename, BUCKET_NAME, "{}_{}".format(SourceFileDict[source_filename],now))
+			return "Log files have been successfully transferred into s3 bucket - {} at {}.".format(BUCKET_NAME, now)
+		except:
+			return "ERROR: Log files have NOT been successfully transferred into s3 bucket." 
